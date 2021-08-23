07f087f864b6e6ff51505abe16d80aab1a41f230
franciscodiaz12 <88333618+franciscodiaz12@users.noreply.github.com>
2021-08-23 00:30:59 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,6 +1,9 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
 
+
+document.addEventListener("DOMContentLoaded", function(e){
+   var boton =  document.getElementById("boton")
+    boton.onclick= window.localStorage.setItem("usuario", "ingresado")
 });
