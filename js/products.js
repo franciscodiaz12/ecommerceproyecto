e66a6b4650419c9f23cee04c61d936c6c6c5a742
franciscodiaz12 <88333618+franciscodiaz12@users.noreply.github.com>
2021-08-22 23:11:45 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,6 +1,33 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+
+
+
+
+document.addEventListener("DOMContentLoaded", async function (e) {
+ 
+  var productos = (await getJSONData(PRODUCTS_URL)).data;
+  const listaProductos = JSON.stringify(productos);
+  var datos = JSON.parse(listaProductos);
+
+  var list = document.createElement("ul");
+  document.body.appendChild(list)
+  for (let i of datos) {
+    var imagen = document.createElement("img");
+    imagen.src = (i.imgSrc);
+    var objeto = document.createElement("li");
+    objeto.innerHTML = "Nombre: " + (i.name) + "</br>" + "Descripción: " + (i.description) + "</br>" + "Costo: " + (i.cost) + "</br>" + "Moneda: " + (i.currency) + "</br>"  + "Vendidos: " + (i.soldCount)
+   list.appendChild(imagen);
+    list.appendChild(objeto);
+    
+  }
+
+
+
+
+
+
 
 });
+
