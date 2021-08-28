c7e0a265e1acce7e718e044dca30bde20ffc2108
franciscodiaz12 <88333618+franciscodiaz12@users.noreply.github.com>
2021-08-27 21:45:42 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -15,14 +15,21 @@ document.addEventListener("DOMContentLoaded", async function (e) {
   var datos = JSON.parse(listaProductos);
 
   var list = document.createElement("ul");
-  list.
+  list.style.alignContent = "left";
+  list.style["list-style-type"]= "none";
+  list.style.fontSize = "large"
+  list.style.fontFamily = "Arial, Helvetica, sans-serif";
   document.body.appendChild(list);
   $(list).appendTo(contenedor);
   for (let i of datos) {
     var imagen = document.createElement("img");
     imagen.src = (i.imgSrc);
+    imagen.style["max-width"]= "60%";
+    imagen.style["border-radius"]= "10px";
+    imagen.style["color"]= "black";
     var objeto = document.createElement("li");
-    objeto.innerHTML = "Nombre: " + (i.name) + "</br>" + "Descripción: " + (i.description) + "</br>" + "Costo: " + (i.cost) + "</br>" + "Moneda: " + (i.currency) + "</br>"  + "Vendidos: " + (i.soldCount)
+    objeto.style.padding = "30px"
+    objeto.innerHTML = "<span style='font-weight: bold;'>Nombre: </span>"   + (i.name) + "</br>" + "<span style='font-weight: bold;'>Descripción: </span>" + (i.description) + "</br>" + "<span style='font-weight: bold;'>Costo: </span>" + (i.cost) + " " + (i.currency) + "</br>"  + "<span style='font-weight: bold;'>Vendidos: </span>" + (i.soldCount)
    list.appendChild(imagen);
     list.appendChild(objeto);
     
