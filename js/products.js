ec45c31378f1e189314d8c4f007ba7a625720c88
franciscodiaz12 <88333618+franciscodiaz12@users.noreply.github.com>
2021-08-29 01:18:15 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -7,21 +7,16 @@ var contenedor = document.getElementsByClassName("container p-5")
 
 document.addEventListener("DOMContentLoaded", async function (e) {
  
-  
+   var productos = (await getJSONData(PRODUCTS_URL)).data;
 
-
-  var productos = (await getJSONData(PRODUCTS_URL)).data;
-  const listaProductos = JSON.stringify(productos);
-  var datos = JSON.parse(listaProductos);
-
-  var list = document.createElement("ul");
-  list.style.alignContent = "left";
-  list.style["list-style-type"]= "none";
-  list.style.fontSize = "large"
-  list.style.fontFamily = "Arial, Helvetica, sans-serif";
-  document.body.appendChild(list);
-  $(list).appendTo(contenedor);
-  for (let i of datos) {
+  var lista = document.createElement("ul");
+  lista.style.alignContent = "left";
+  lista.style["list-style-type"]= "none";
+  lista.style.fontSize = "large";
+  lista.style.fontFamily = "Arial, Helvetica, sans-serif";
+  document.body.appendChild(lista);
+  $(lista).appendTo(contenedor);
+  for (let i of productos) {
     var imagen = document.createElement("img");
     imagen.src = (i.imgSrc);
     imagen.style["max-width"]= "60%";
@@ -30,8 +25,8 @@ document.addEventListener("DOMContentLoaded", async function (e) {
     var objeto = document.createElement("li");
     objeto.style.padding = "30px"
     objeto.innerHTML = "<span style='font-weight: bold;'>Nombre: </span>"   + (i.name) + "</br>" + "<span style='font-weight: bold;'>Descripción: </span>" + (i.description) + "</br>" + "<span style='font-weight: bold;'>Costo: </span>" + (i.cost) + " " + (i.currency) + "</br>"  + "<span style='font-weight: bold;'>Vendidos: </span>" + (i.soldCount)
-   list.appendChild(imagen);
-    list.appendChild(objeto);
+   lista.appendChild(imagen);
+    lista.appendChild(objeto);
     
   }
 
